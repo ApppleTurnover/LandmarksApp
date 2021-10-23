84ab3af2eb8ee55ed3cd23ac43a4ae110d66c90b
ApppleTurnover <apppleturnover@gmail.com>
2021-10-23 14:41:35 +1000

    Initial Commit

---
+++ SomeThing/SomeThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeThingApp.swift
+//  SomeThing
+//
+//  Created by Максим Рюхманов on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SomeThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
