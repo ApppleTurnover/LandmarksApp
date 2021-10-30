68666dd9ba3145702c8b767c09ae2f9b358acc03
ApppleTurnover <apppleturnover@gmail.com>
2021-10-30 21:28:10 +1000

    Completed Apple tutorial

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct SomeThingApp: App {
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
